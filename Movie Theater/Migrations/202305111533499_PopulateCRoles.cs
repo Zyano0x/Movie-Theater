00a54ea90b47a874328b8fb525b8fa3ee0dbd9d6
N.T.Hoai <nthdm00@gmail.com>
2023-05-11 23:34:05 +0700

    Update

---
+++ Movie Theater/Migrations/202305111533499_PopulateCRoles.cs
@@ -0,0 +1,20 @@
+﻿namespace Movie_Theater.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCRoles : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CRoles (Name) VALUES (N'Actor')");
+            Sql("INSERT INTO CRoles (Name) VALUES (N'Director')");
+            Sql("INSERT INTO CRoles (Name) VALUES (N'Producer')");
+            Sql("INSERT INTO CRoles (Name) VALUES (N'Writer')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
