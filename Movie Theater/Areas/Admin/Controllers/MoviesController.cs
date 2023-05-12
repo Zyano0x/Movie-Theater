831ea3bc904e08a20e4bcad375f639c17f36cd3c
N.T.Hoai <nthdm00@gmail.com>
2023-05-12 17:54:25 +0700

    Update Movie Schedule

--- Movie Theater/Areas/Admin/Controllers/MoviesController.cs
+++ Movie Theater/Areas/Admin/Controllers/MoviesController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 
 namespace Movie_Theater.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Staff, Adminstrator")]
     public class MoviesController : Controller
     {
         ApplicationDbContext _dbContext = new ApplicationDbContext();
